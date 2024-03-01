cb0f9e95d16fbcec0ff9a961167238b6dfe6e9ac
Hushruz <olimnazarov.khushruz@gmail.com>
2024-03-01 14:14:28 +0500

    Проект создан,начало работы2

--- coco-app/app/index.js
+++ coco-app/app/index.js
@@ -4,6 +4,7 @@ import { Link } from 'expo-router';
 export default function Page() {
   return (
     <View>
+    <Text>App worked good</Text>
       <Link href="/onboarding/">Onboarding</Link>
       {/* ...other links */}
     </View>
